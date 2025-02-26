009fe8bd5bca37283fd7712bafb0bbca99400f2c
Hürra Aras <hyrraaras@gmail.com>
2025-02-26 21:17:33 +0300

    26.02 algoritma dersi
    derste yazılan kodlar

---
+++ Pen.java
@@ -0,0 +1,10 @@
+package ders;
+
+public class Pen {
+
+	String color;
+	int height;
+	void Writemassage(String message) {
+		System.out.println(message);
+	}
+}
---
+++ PenFactor.java
@@ -0,0 +1,20 @@
+package ders;
+
+public class PenFactor {
+
+	public static void main(String[] args) {
+	 Pen firstpen =new Pen();
+	 String message="First pen producted";
+	 firstpen.color="black";
+	 firstpen.height=12;
+	 firstpen.Writemassage(message);
+	 Pen secondpen =new Pen();
+	 String message1="Second pen producted";
+	 secondpen.color="purple";
+	 secondpen.height=19;
+	 secondpen.Writemassage(message1);
+System.out.println("Firstpen color: " +firstpen.color+"firstpen hight: "+firstpen.height+ "+ secondpen color:"  +secondpen.color+"secondpen hight: "+secondpen.height );
+
+	}
+
+}
